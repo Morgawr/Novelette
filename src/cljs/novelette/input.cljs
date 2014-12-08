1cbfe8f50fc9fe4fb14a45bf076d9270be3837f1
Morgawr <morgawr@gmail.com>
2014-12-08 23:03:49 +0000

    fixed absolut position bug for mouse

--- src/cljs/novelette/input.cljs
+++ src/cljs/novelette/input.cljs
@@ -1,17 +1,22 @@
-(ns novelette.input)
+(ns novelette.input
+  (:require [goog.events.EventType :as event-type]
+            [goog.events :as events]))
 
 (def MOUSE-STATE (atom {:x 0
                         :y 0
                         :clicked false
                         }))
 
+(def BOUNDING-BOX (atom nil))
+
 ; TODO - Maybe add keyboard stuff too
-; TODO - Make mouse work with relative position to top-left corner
 (defn mouse-move-listener
   [event]
-  (swap! MOUSE-STATE assoc
-         :x (.-clientX event)
-         :y (.-clientY event)))
+  (if (nil? @BOUNDING-BOX)
+    (.log js/console "WARNING: Input not properly initialized.")
+    (let [x (- (.-clientX event) (.-left @BOUNDING-BOX))
+          y (- (.-clientY event) (.-top @BOUNDING-BOX))]
+      (swap! MOUSE-STATE assoc :x x :y y))))
 
 (defn mouse-click-listener
   [event]
@@ -20,3 +25,9 @@
 (defn mouse-declick
   []
   (swap! MOUSE-STATE assoc :clicked false))
+
+(defn init
+  [canvas]
+  (reset! BOUNDING-BOX (.getBoundingClientRect canvas))
+  (events/listen canvas event-type/MOUSEMOVE mouse-move-listener)
+  (events/listen canvas event-type/CLICK mouse-click-listener))
