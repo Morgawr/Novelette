7f08771d5edf8b96e3471c7a9c94f18ee01baf19
Morgawr <morgawr@gmail.com>
2016-05-06 22:11:03 +0100

    Fixed dependency issues and upgraded to clojure 1.8.0+cljs

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject novelette "0.1.0-indev"
   :description "ClojureScript engine for visual novels."
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.170"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.51"]
                  [prismatic/schema "1.0.1"]
                  [lein-doo "0.1.6"]
                  [novelette-text "0.1.3"]
                  [novelette-sprite "0.1.0.1"]]
-  :plugins [[lein-cljsbuild "1.1.1"]
+  :plugins [[lein-cljsbuild "1.1.3"]
             [lein-doo "0.1.6"]]
   :hooks [leiningen.cljsbuild]
   :clean-targets ["runtime/js/*"]
@@ -17,11 +17,13 @@
     {:id "novelette"
      :source-paths ["src/"]
      :compiler
-     {:optimizations :whitespace
+     {:optimizations :simple
       :output-dir "runtime/js"
       :output-to  "runtime/js/novelette.js"
       :pretty-print true
-      :source-map "runtime/js/novelette.js.map"}}
+      :source-map "runtime/js/novelette.js.map"
+      :closure-output-charset "US-ASCII"
+      }}
     {:id "tests"
      :source-paths ["src/" "tests"]
      :compiler {:output-to "compiled-tests/tests.js"
